1b3489bd704ee8afa6662cdb1c24f87147164e74
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-11 09:07:56 +0600

    typescript type

---
+++ type.js
@@ -0,0 +1,15 @@
+var firstCharacter = "Jonas Kahnwald";
+var firstCharacterRole = "Protagonist";
+console.log(firstCharacter, firstCharacterRole);
+var secondCharacter = "Bartosz Tiedemann";
+var secondCharacterRole = "Deuteragonist";
+console.log(secondCharacter, secondCharacterRole);
+secondCharacterRole = "Tertiary";
+console.log(secondCharacter, secondCharacterRole);
+var age = 24;
+var gpa = 4.5;
+var isBlack = false;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(5));
---
+++ type.ts
@@ -0,0 +1,25 @@
+const firstCharacter = "Jonas Kahnwald"
+const firstCharacterRole = "Protagonist"
+
+console.log(firstCharacter, firstCharacterRole)
+
+
+const secondCharacter = "Bartosz Tiedemann"
+let secondCharacterRole = "Deuteragonist"
+
+console.log(secondCharacter, secondCharacterRole);
+
+secondCharacterRole = "Tertiary"
+
+console.log(secondCharacter, secondCharacterRole)
+
+let age = 24
+let gpa = 4.5
+let isBlack = false
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI
+}
+
+console.log(circ(5));
+
