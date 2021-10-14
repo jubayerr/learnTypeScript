7c3f5b7cc8f54eb8270bf4a6e30668ada493f1bb
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-14 10:55:58 +0600

    tuple

---
+++ public/tuples.js
@@ -0,0 +1,6 @@
+"use strict";
+// Tuples
+let a = [3, 'hello', { p: 3 }];
+let b;
+b = [3, 'hello', { r: 2 }];
+b[0] = 5;
---
+++ src/tuples.ts
@@ -0,0 +1,9 @@
+// Tuples
+
+let a = [3, 'hello', {p: 3}]
+
+let b:[number, string, object]
+
+b = [3, 'hello', {r:2}]
+
+b[0] = 5;
